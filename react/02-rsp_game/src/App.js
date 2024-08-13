ddcc5bb73cbcd4e617e7ede85382fc93995059ab
je <wjddmsy12345@gmail.com>
2024-08-13 22:06:05 +0900

    0813

--- react/02-rsp_game/src/App.js
+++ react/02-rsp_game/src/App.js
@@ -1,6 +1,49 @@
 import "./App.css";
+import reset from "./assets/ic-reset.svg";
+import { useState } from "react";
+import HandIcon from "./HandIcon";
 function App() {
-  return <div className="App"></div>;
+  const [score, setScore] = useState(0);
+  return (
+    <div className="App">
+      <div>
+        <h1 className="App-title">가위바위보</h1>
+        <img src={reset} className="reset" />
+        <div className="score">
+          <div className="count">
+            <div className="number">0</div>
+            <div>나</div>
+          </div>
+          <div className="ver">:</div>
+          <div className="count">
+            <div className="number">0</div>
+            <div> 상대</div>
+          </div>
+        </div>
+        <div className="rsp">
+          <div>
+            <HandIcon />
+          </div>
+          <div>vs</div>
+          <div>이미지</div>
+        </div>
+        <div className="bet">
+          <span>배점 </span>
+          <input type="number"></input>
+          <span> 배</span>
+        </div>
+        <div className="history">
+          <h2>승부기록</h2>
+          <p>승&패</p>
+        </div>
+        <div className="btn">
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
